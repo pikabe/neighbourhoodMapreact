b494468bd3ecbbf151da07c589761a91c6b3d605
Gopika Bejoy <gopikabejoy123@gmail.com>
2018-08-09 00:29:42 +0100

    Added input box

--- src/SideBar.js
+++ src/SideBar.js
@@ -3,18 +3,51 @@ import './App.css';
 
 
 class SideBar extends Component {
+  state={
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+  }
+
+
+  /*BooksAPI.search(query).then((searchResults) => {
+    if (query.length>0) {//checks if searchResults has stored any books
+      this.setState({
+        searchResults
+      })}
+     else {
+      this.setState({
+        searchResults: []
+      })
+    }
+  })*/
+
   render(){
     const styles ={width:'100%',height:'80%'}
     let sideBar = "sideBar " + this.props.toggle
     return(
-
       <div className={sideBar} >
+      <input className="input"
+              type="text"
+              placeholder="Search Location"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+      {
+            (this.props.locationsList.constructor === Array) ?
+                this.props.locationsList.map(location =>
+            <div className="location-sideBar" key={location.a}> location.a </div>
+            ):null
+          }
+        {/*<div className="location-sideBar"> h1 </div>
         <div className="location-sideBar"> h1 </div>
         <div className="location-sideBar"> h1 </div>
         <div className="location-sideBar"> h1 </div>
         <div className="location-sideBar"> h1 </div>
-        <div className="location-sideBar"> h1 </div>
-        <div className="location-sideBar"> h1 </div>
+        <div className="location-sideBar"> h1 </div>*/}
 
       </div>
 
